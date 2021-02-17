a1469a60a477d628714bb7d2848a1545035963ca
Allen <jihoshin.28@gmail.com>
2021-02-16 18:26:49 -0800

    added routing and pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from './pages/home/home.component'
+import {AboutComponent} from './pages/about/about.component'
+import {TodosComponent} from './pages/todos/todos.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'todo_list', component: TodosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TodosComponent } from './components/todos/todos.component';
+import { TodosComponent } from './pages/todos/todos.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './pages/about/about.component';
+import { HomeComponent } from './pages/home/home.component';
+import { TodoContainerComponent } from './components/todo-container/todo-container.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodosComponent
+    TodosComponent,
+    NavbarComponent,
+    AboutComponent,
+    HomeComponent,
+    TodoContainerComponent
   ],
   imports: [
     BrowserModule,
